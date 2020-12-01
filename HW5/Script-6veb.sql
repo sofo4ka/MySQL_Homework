bd13a300b7713edf878a17dab7ebc443a99d0552
sofa.shvaryova <sofa.shvaryova@gmail.com>
2020-12-01 19:14:01 +0300

    added 5 homework

---
+++ HW5/Script-6veb.sql
@@ -0,0 +1,146 @@
+-- ѕерсональна€ страница пользовател€
+select 
+	firstname,
+	lastname,
+	hometown,
+	(select filename from photos where id = u.photo_id) as 'personal_photo',
+	(select count(*) from (select initiator_user_id from friend_requests where target_user_id = u.id and status = 'approved'
+		union
+	select target_user_id from friend_requests where initiator_user_id = u.id and status = 'approved') as fr_list) as 'friends',
+	(select count(*) from friend_requests where target_user_id = u.id and status ='requested') as 'followers',
+	(select count(*) from photos where user_id = u.id) 'photos'
+from users as u 
+where id = 21;
+
+-- —писок друзей пользовател€ 1 с указанием пола и возраста 
+
+select  
+	firstname,
+	lastname,
+	timestampdiff(year, birthday, now()) as age,
+	case(gender)
+		when 'm' then 'ћужчина'
+		when 'f' then '∆енщина'
+	end as 'gender'
+from users where id in (select * from (
+select 
+	case
+		when initiator_user_id = 1 and status = 'approved' then target_user_id
+		when target_user_id = 1 and status = 'approved' then initiator_user_id
+	end as friend_id
+from friend_requests) as fr_list where friend_id is not null);
+
+
+-- ѕолучить id друзей пользовател€ номер 1
+select initiator_user_id from friend_requests where target_user_id = 1 and status = 'approved'
+union
+select target_user_id from friend_requests where initiator_user_id = 1 and status = 'approved';
+
+-- ѕолучить id друзей пользовател€ номер 2
+select * from (
+select 
+	case
+		when initiator_user_id = 1 and status = 'approved' then target_user_id
+		when target_user_id = 1 and status = 'approved' then initiator_user_id
+	end as friend_id
+from friend_requests) as fr_list where friend_id is not null;
+
+-- непрочитанные сообщени€, адресованные пользователю (список)
+select
+	(select concat(firstname, ' ', lastname) from users where id = m.from_user_id) from_user,
+	message,
+	create_at 
+from messages m
+where to_user_id = 30 and is_read = 0;
+
+-- кол-во непрочитанных сообщений, адресованных пользователю 30 от друзей
+select
+	(select concat(firstname, ' ', lastname) from users where id = m.from_user_id) from_user,
+	count(*) as total_msg 
+from messages m
+where 
+	from_user_id in (select initiator_user_id from friend_requests where target_user_id = m.to_user_id and status = 'approved'
+		union
+	select target_user_id from friend_requests where initiator_user_id = m.to_user_id and status = 'approved') 
+and
+	to_user_id = 30 
+and is_read = 0 
+group by m.from_user_id order by total_msg desc;
+
+
+
+-- среднее кол-во постов, опубликованных каждым пользователем
+
+select avg(total_user_posts) from (select count(*) as total_user_posts  from posts group by user_id) as total_users_posts_tbl;
+
+-- архив новостей
+select count(*) as total_news, monthname(created_at) as `month` from posts group by `month` order by total_news desc;
+
+-- среднее количество групп у всех пользователей
+select (select count(*) from users_communities)/(select count(*) from users);
+
+-- кол-во пользователей на число групп с ненулевым числом пользлвателей
+select (select count(*) from users)/(select count(distinct community_id ) from users_communities);
+
+--  min max
+select max(timestampdiff(year, birthday, now())) as age from users;
+
+select timestampdiff(year, birthday, now()) as age from users order by age desc limit 1;
+
+select gender, min(timestampdiff(year, birthday, now())) as age from users group by gender;
+
+-- агрегирующие функции и null
+select count(*) from photos;
+
+select count(id) from photos;
+
+select count(description) from photos;
+
+
+-- ѕусть задан некоторый пользователь. »з всех друзей этого пользовател€ найдите человека, который больше всех общалс€ с нашим пользователем.
+-- (если € правильно пон€ла, то общалс€ это значит, что его сообщени€ читали)
+
+select
+	(select concat(firstname, ' ', lastname) from users where id = m.from_user_id) from_user,
+	count(*) as total_msg 
+from messages m
+where 
+	from_user_id in (select initiator_user_id from friend_requests where target_user_id = m.to_user_id and status = 'approved'
+		union
+	select target_user_id from friend_requests where initiator_user_id = m.to_user_id and status = 'approved') 
+and
+	to_user_id = 1 
+and is_read = 1 
+group by m.from_user_id order by total_msg desc limit 1;
+
+
+-- ѕодсчитать общее количество лайков, которые получили 10 самых молодых пользователей.
+
+select 
+	(select user_id from posts where id = p.post_id) user_id,
+	count(*) as total_postlikes 
+from postslikes p 
+	where likeexists = 1 
+group by p.post_id order by total_postlikes desc;
+
+
+
+
+
+
+select 
+	(select concat(firstname, ' ', lastname), timestampdiff(year, birthday, now()) as age 
+	from users where id = p.user_id)
+	(select user_id from posts where id = p.post_id) user_id,
+	count(*) as total_postlikes 
+from postslikes p where likeexists = 1 
+group by age order by total_postlikes desc;
+
+
+
+-- ќпределить кто больше поставил лайков (всего) - мужчины или женщины?
+
+
+
+-- Ќайти 10 пользователей, которые про€вл€ют наименьшую активность в использовании социальной сети.
+
