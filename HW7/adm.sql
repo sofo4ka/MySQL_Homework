d0ba23d4d2fb27a817579509bb8331b814df270a
sofa.shvaryova <sofa.shvaryova@gmail.com>
2020-12-11 14:52:05 +0300

    upgraded adm.sql

--- HW7/adm.sql
+++ HW7/adm.sql
@@ -5,6 +5,7 @@ use shop;
 drop user if exists 'shop'@'localhost';
 create user 'shop'@'localhost';
 grant all on shop.* to 'shop'@'localhost';
+grant grant option on shop.* to 'shop'@'localhost';
 
 create user if not exists 'shop_read'@'localhost';
 grant select on shop.* to 'shop_read'@'localhost';
